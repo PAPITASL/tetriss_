edec4f8b87ccfae6c9ae774f9df1fc216128201a
Paula <paula1007002@gmail.com>
2023-08-21 14:56:04 -0500

    feat: :sparkles:

---
+++ lib/bricks.ex
@@ -0,0 +1,12 @@
+defmodule Tetris.Brick do
+  defstruct [
+    name: :i,
+    location: {40, 0},
+    rotation: 0,
+    reflection: false
+  ]
+
+  def new() do
+    __struct__
+  end
+end
---
+++ test/brick_test.exs
@@ -0,0 +1,14 @@
+defmodule BrickTest do
+  use ExUnit.Case
+
+  import Tetris.Brick
+
+  test "Create a new brick" do
+    assert new_brick().name == :i
+
+  end
+
+  def new_brick do
+    new()
+  end
+end
